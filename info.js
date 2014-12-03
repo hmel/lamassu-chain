b8dfc715941c74ce8190dec7e62f90df193abede
Josh Harvey <josh@lamassu.is>
2014-12-02 20:39:54 -0500

    new api

--- info.js
+++ info.js
@@ -1,64 +1,28 @@
 'use strict';
 
+// WIP: This does a very superficial test to see if funds have arrived.
+// Will be continiously improved.
+
+var _ = require('lodash');
 var chain = require('chain-node');
 
 exports.NAME = 'Chain.com';
 exports.SUPPORTED_MODULES = ['info'];
 
-
-function countAll(array, address) {
-  return array.reduce(function(prev, current) {
-    if (current.addresses.indexOf(address) !== -1)
-      prev += current.value;
-
-    return prev;
-  }, 0);
-}
-
-// Iterates all in/outputs and counts how given tx changed address's balance
-function getAmountForAddress(address, tx) {
-  return countAll(tx.outputs, address) - countAll(tx.inputs, address);
-}
-
-function processTx(tx, address) {
-  return {
-    txHash: tx.hash,
-    tsReceived: Date.parse(tx.chain_received_at),
-    confirmations: tx.confirmations,
-    amount: getAmountForAddress(address, tx),
-    fees: tx.fees,
-
-    // NOTE: optional, shold be set when plugin implements 0-conf
-    authorized: false,
-    confidence: 0.0
-  };
-}
-
-
-exports.config = function config(localConfig) {
-  if (localConfig) {
-    chain.apiKeyId = localConfig.keyId;
-    chain.apiKeySecret = localConfig.keySecret;
-  }
+exports.config = function config(_config) {
+  chain.apiKeyId = _config.apiKeyId;
+  chain.apiKeySecret = _config.apiKeySecret;
 };
 
-exports.getAddressLastTx = function getAddressLastTx(address, callback) {
-  chain.getAddressTransactions(address, {limit: 1}, function(err, resp) {
-    if (err)
-      return callback(err);
-
-    if (!resp.length)
-      return callback(); // no error & no tx
-
-    callback(null, processTx(resp[0], address));
-  });
-};
-
-exports.getTx = function getTx(txHash, address, callback) {
-  chain.getTransaction(txHash, function(err, resp) {
-    if (err)
-      return callback(err);
-
-    callback(null, processTx(resp, address));
+exports.checkAddress = function checkAddress(address, amount, cb) {
+  chain.getAddressTransactions(address, function(err, resp) {
+    if (err) return cb(err);
+    if (resp.length === 0) return cb(null, 'notSeen', 0, null);
+    var tx = resp[0];
+    var output = _.find(tx.outputs, {addresses: [address]});
+    if (!output) return cb(null, 'notSeen', 0, null);
+    var txAmount = output.value;
+    var status = txAmount < amount ? 'insufficientFunds' : 'authorized';
+    cb(null, status, amount, tx.hash);
   });
 };
